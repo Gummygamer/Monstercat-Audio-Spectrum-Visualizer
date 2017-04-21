d7eee7f7d4e825ae91e154b7bf0f44bcb457d870
Gummygamer <aisushin@gmail.com>
2017-04-20 21:40:41 -0300

    Update songs.js

--- js/songdata/songs.js
+++ js/songdata/songs.js
@@ -1,5 +1,5 @@
 //AddSong("Artist Name","Song Name","Genre Name","File lovation in terms of songs/","Image location","Album Name")
 //    "Album Name" should be the same file in img/albums/ to display the image
 
-AddSong("Aero Chord","4U","Moombahton","music.mp3","img/artists/aerochord.png","4usingle")
+AddSong("?","?","Peace","music.mp3","img/artists/aerochord.png","4usingle")
 
